631c83dbb242737249830dc20208d38aba853633
mrhoodz <hoodzt7@gmail.com>
2022-12-27 01:49:56 +0200

    six commit

--- pages/_firstpage/email.js
+++ pages/_firstpage/email.js
@@ -1,31 +1,31 @@
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
+// // using Twilio SendGrid's v3 Node.js Library
+// // https://github.com/sendgrid/sendgrid-nodejs
 
-// import {sgMail} from "@sendgrid/mail"
+// // import {sgMail} from "@sendgrid/mail"
 
-  function sender(to) {
-  const sgMail = require("@sendgrid/mail");
-  sgMail.setApiKey(
-    "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"
-  );
-  const msg = {
-    to: to, // Change to your recipient
-    from: "tinotendam@juggleafrica.com", // Change to your verified sender
-    subject: "Ok lm out of hope now lol",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  };
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log("Email sent");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
+//   function sender(to) {
+//   const sgMail = require("@sendgrid/mail");
+//   sgMail.setApiKey(
+//     "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"
+//   );
+//   const msg = {
+//     to: to, // Change to your recipient
+//     from: "tinotendam@juggleafrica.com", // Change to your verified sender
+//     subject: "Ok lm out of hope now lol",
+//     text: "and easy to do anywhere, even with Node.js",
+//     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+//   };
+//   sgMail
+//     .send(msg)
+//     .then(() => {
+//       console.log("Email sent");
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
 
-sender("realwebsdigital@gmail.com");
+// sender("realwebsdigital@gmail.com");
 
 
 
