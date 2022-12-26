14b983b8e6395fc2d6530588b1d288114cfb8fc9
mrhoodz <hoodzt7@gmail.com>
2022-12-27 01:46:25 +0200

    fifth commit

--- pages/api/mailer.js
+++ pages/api/mailer.js
@@ -1,13 +1,13 @@
-const mailchimp = require("@mailchimp/mailchimp_marketing");
+// const mailchimp = require("@mailchimp/mailchimp_marketing");
 
-mailchimp.setConfig({
-  apiKey: "6e7a8e880c52ff5b057bea4bd3af5c53-us21",
-  server: "https://us21.admin.mailchimp.com",
-});
+// mailchimp.setConfig({
+//   apiKey: "6e7a8e880c52ff5b057bea4bd3af5c53-us21",
+//   server: "https://us21.admin.mailchimp.com",
+// });
 
-async function run() {
-  const response = await mailchimp.ping.get();
-  console.log(response);
-}
+// async function run() {
+//   const response = await mailchimp.ping.get();
+//   console.log(response);
+// }
 
-run()
+// run()
